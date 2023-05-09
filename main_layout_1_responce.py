2037cf80fce210d44fac578e68a5cdb7a0a1bf94
PyScientist <dsa.msk@gmail.com>
2023-05-10 00:08:36 +0700

    Refactoring

--- main_layout_1_responce.py
+++ main_layout_1_responce.py
@@ -8,23 +8,30 @@ from PySide6.QtSql import QSqlTableModel
 from layout_1_responce import Ui_MainWindow
 
 
-class QC(QMainWindow):
+class QC(QMainWindow, Ui_MainWindow):
     def __init__(self):
-        super(QC, self).__init__()
-        self.ui = Ui_MainWindow
-        self.ui.setupUi(self)
-        self.log_out_btn(self)
+        super().__init__()
+        self.setupUi(self)
+        self.log_out_btn_adjust()
+        #self.btn_start_stop_scan.setCheckable(True)
+        #self.btn_start_stop_scan.clicked.connect(self.hide_top_layout)
 
-    def log_out_btn(self):
-        text = ui.btn_log_out.text('jkdhdsbfvcx')
+    def log_out_btn_adjust(self):
+        text = self.btn_log_out.setText('log out button')
+        self.btn_log_out.setCheckable(True)
+        self.btn_log_out.clicked.connect(self.hide_top_layout)
+
         return text
 
+    def hide_top_layout(self):
+        if self.btn_log_out.isChecked():
+            self.horizontalFrame_core_and_cconsole.show()
+        else:
+            self.horizontalFrame_core_and_cconsole.hide()
 
 
 if __name__ == "__main__":
-    app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
+    app = QApplication(sys.argv)
+    qc = QC()
+    qc.show()
     sys.exit(app.exec())
